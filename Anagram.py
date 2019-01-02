0853686ab747f09988311482a50eea324a909899
Isabella <ifederico>
2019-01-02 14:32:21 +0000

    Add start up code

---
+++ Anagram.py
@@ -0,0 +1,62 @@
+#supplied with a word and identify anagrams to that word
+#easy and hard levels based on amount of letters
+#hint based on the definition of the anagram listed
+#    ex. "cinema" *there are 2 anagrams*
+#    listing how many anagrams there are; part of difficulty or choose anagrams that have a single anagram
+#25 words per level
+#4 letters per anagram in easy mode
+#6 or more per anagram in hard mode
+
+class Game_Basics:
+    def __init__():
+        self.player = player
+
+class Easy:
+
+easy_guess_words = ['deaf'fade,'dice'iced,'disk'kids/skid,'sole'lose,
+'golf'flog,'fuel'flue,'grab'brag/garb,'gulp'plug,'fowl'wolf/flow,'dear'read/dare,'snag'nags,'mean'amen/mane/name,
+'note'tone,'pace'cape,'pale'leap/peal/plea,'race'care/acre,'reef'free,'ring'grin,'silo'soil/oils,'step'pets/pest]
+
+
+class Hard:
+
+hard_guess_words = ['cinema'iceman/anemic,'except'expect,'recede'decree,'friend'finder,
+'grease'agrees,'incept'pectin,'leader'dealer,'marine'airmen/remain,'meteor'remote,'oceans'canoes,'sniper'ripens,'hustle'sleuth,
+'fringe'finger,'impart'armpit,'listen'silent/enlist/tinsel/inlets,'manors'ransom,'plates'palest/pastel/petals/pleats/staple,
+'potion'option,'resins'rinses/sirens,'rustic'citrus]
+
+
+input = print("Welcome to Fanagrams! Get it? Cuz the creator is a 'fan' of 'anagrams'! AHAHA I'm too much.... You're probably thinking, what even are anagrams? If you want to play, type 'continue'. If not, type 'nope'.")
+    if input = 'continue':
+        print("Anagrams are the rearrangements of letters from a word to create another word. It simple word play at its best!")
+        print("Fanagrams is designed with two levels: Easy and Hard")
+        print("Easy consists of 4 letter words while Hard consists of 6 letter words. You get the option to choose either level!")
+        input2 = print("So what will it be, Easy or Hard?")
+            if input2 = 'easy'
+                shift to easy mode
+            else:
+                shift to hard mode
+        print("To play, you must think of an anagram per word (of ten words) in fifteen seconds! Don't worry if you're stumped, I'll hint you once to put you in the right direction!")
+        print("P.S. Some of the words may have several anagrams and each hint will be specific to one option. Soooo...... ARE YOU READY? Type 'start'.")
+    else:
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
