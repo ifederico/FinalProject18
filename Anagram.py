a9803a3d588e4a74657f3dfb83d1c02c078766fc
Isabella <ifederico>
2019-01-16 15:50:04 +0000

    Smaller additions

--- Anagram.py
+++ Anagram.py
@@ -2,6 +2,7 @@ import random
 import sys
 
 chosen_word = ""
+guesses = ""
 
 class Game_Basics:
     "'Generic game basics'"
@@ -66,27 +67,29 @@ def end_game(self):
     return point_track
 
 #easy
-easy_words = {'deaf':'fade','dice':'iced','disk':'kids'and'skid','sole':'lose','golf':'flog','fuel':'flue','grab':'brag'and'garb',
-'gulp':'plug','fowl':'wolf'and'flow','dear':'read'and'dare','snag':'nags','mean':'amen'and'mane'and'name','note':'tone','pace':'cape',
-'pale':'peal'and'plea'and'leap','race':'care'and'acre','reef':'free','ring':'grin','silo':'soil'and'oils','step':'pets'and'pest'}
+easy_words = {'deaf':['fade'],'dice':['iced'],'disk':['kids','skid'],'sole':['lose'],'golf':['flog'],'fuel':['flue'],'grab':['brag','garb'],
+'gulp':['plug'],'fowl':['wolf','flow'],'dear':['read','dare'],'snag':['nags'],'mean':['amen','mane','name'],'note':['tone'],'pace':['cape'],
+'pale':['peal','plea','leap'],'race':['care','acre'],'reef':['free'],'ring':['grin'],'silo':['soil','oils'],'step':['pets','pest']}
 
 #hard
-hard_words = {'cinema':'iceman'and'anemic','except':'expect','recede':'decree','friend':'finder','grease':'agrees','incept':'pectin',
-'leader':'dealer','marine':'airmen'and'remain','meteor':'remote','oceans':'canoes','sniper':'ripens','hustle':'sleuth','fringe':'finger',
-'impart':'armpit','listen':'silent'and'enlist'and'tinsel'and'inlets','manors':'ransom','plates':'palest'and'pastel'and'petals'and'pleats'and'staple',
-'potion':'option','resins':'rinses'and'sirens','rustic':'citrus'}
+hard_words = {'cinema':['iceman','anemic'],'except':['expect'],'recede':['decree'],'friend':['finder'],'grease':['agrees'],'incept':['pectin'],
+'leader':['dealer'],'marine':['airmen','remain'],'meteor':['remote'],'oceans':['canoes'],'sniper':['ripens'],'hustle':['sleuth'],'fringe':['finger'],
+'impart':['armpit'],'listen':['silent','enlist','tinsel','inlets'],'manors':['ransom'],'plates':['palest','pastel','petals','pleats','staple'],
+'potion':['option'],'resins':['rinses','sirens'],'rustic':['citrus']}
 
 
-input1 = input("Welcome to Fanagrams! Get it? Cuz the creator is a 'fan' of 'anagrams'! AHAHA I'm too much.... You're probably thinking, what even are anagrams? If you want to play, type 'c' for continue. If not, type 'n' for nope.")
+input1 = input("Welcome to Fanagrams! Get it? Cuz the creator is a 'fan' of 'anagrams'! AHAHA I'm too much.... You're probably thinking, what even are anagrams? If you want to play, type 'c' for continue. If not, type 'n' for nope. ")
 while input1 not in ['c', 'n']:
-  input1 = input("If you want to play, type 'c' for continue. If not, type 'n' for nope.")
+  input1 = input("If you want to play, type 'c' for continue. If not, type 'n' for nope. ")
+if input1 == 'n':
+  sys.exit()
 if input1 == 'c':
   print("Anagrams are the rearrangements of letters from a word to create another word. It's simple word play at its best!")
   print("Fanagrams is designed with two levels: Easy and Hard")
   print("Easy consists of 4 letter words while Hard consists of 6 letter words. You get the option to choose either level!")
-input2 = input("So what will it be, Easy or Hard?")
+input2 = input("So what will it be, Easy or Hard? ")
 while input2 not in ['easy','hard']:
-  input2 = input("So what will it be Easy or Hard?")
+  input2 = input("So what will it be Easy or Hard? ")
 if input2 == 'easy':
   print("To play, you must think of an anagram per word (of ten words) in fifteen seconds! Don't worry if you're stumped, I'll hint you once to put you in the right direction!")
   print("P.S. Some of the words may have several anagrams and each hint will be specific to one option. ARE YOU READY?")
@@ -95,5 +98,12 @@ elif input2 == 'hard':
   print("To play, you must think of an anagram per word (of ten words) in ten seconds! Don't worry if you're stumped, I'll hint you once to put you in the right direction!")
   print("P.S. Some of the words may have several anagrams and each hint will be specific to one option. ARE YOU READY?")
   chosen_word = random_word(hard_words)
-if input1 == 'n':
-  sys.exit()
+
+anagram_choice = input(f"Anagram: {guesses}")
+
+
+
+
+
+
+
